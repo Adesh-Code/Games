5122ec4a9c8bb7b736e2ddb4dfd71a68f464ebec
adesh <>
2024-06-20 18:12:24 +0530

    Added two player

--- lib/screens/atom_reaction.dart
+++ lib/screens/atom_reaction.dart
@@ -12,8 +12,10 @@ class AtomReaction extends StatefulWidget {
 class _AtomReactionState extends State<AtomReaction> {
   final double _iconSize = 50;
   final (int, int) totalTiles = (8, 8);
-  bool gameEnd = false;
-  List<List<int>> world = [];
+  bool? _winner;
+  List<List<String>> world = [];
+  bool loading = false;
+  bool _playerOne = true;
 
   @override
   void initState() {
@@ -25,62 +27,88 @@ class _AtomReactionState extends State<AtomReaction> {
     for (int i = 0; i < totalTiles.$1; i++) {
       world.add([]);
       for (int j = 0; j < totalTiles.$2; j++) {
-        world[i].add(0);
+        world[i].add('0');
       }
     }
   }
 
-  void _selectTile((int, int) position) {
+  void _selectTile((int, int) position, bool recursive, bool playerOne) {
+    final prefix = playerOne == false ? '-' : '';
+
+    // Should not be able to place atom on wrong player side
+    if (recursive == false && !(world[position.$1][position.$2] == '0')) {
+      if (!(world[position.$1][position.$2].contains('-')) && prefix == '-') {
+        return;
+      }
+      if (world[position.$1][position.$2].contains('-') && prefix == '') {
+        return;
+      }
+    }
+
     switch (world[position.$1][position.$2]) {
-      case 0:
-        world[position.$1][position.$2] = 1;
-      case 1:
+      case '0':
+        world[position.$1][position.$2] = "${prefix}1";
+      case '-1':
+      case '1':
         switch (_getTilePos(position)) {
           case TilePos.center:
           case TilePos.side:
-            world[position.$1][position.$2] = 2;
+            world[position.$1][position.$2] = '${prefix}2';
             break;
           case TilePos.corner:
-            _blastAtom(position);
+            _blastAtom(position, playerOne);
             break;
         }
         break;
-      case 2:
+      case '2':
+      case '-2':
         switch (_getTilePos(position)) {
           case TilePos.center:
-            world[position.$1][position.$2] = 3;
+            world[position.$1][position.$2] = '${prefix}3';
           case TilePos.side:
-            _blastAtom(position);
+            _blastAtom(position, playerOne);
             break;
           case TilePos.corner:
             break;
         }
         break;
-      case 3:
-        _blastAtom(position);
+      case '3':
+      case '-3':
+        _blastAtom(position, playerOne);
         break;
     }
-    setState(() {});
+    setState(() {
+      if (recursive) {
+        return;
+      }
+      _playerOne = !_playerOne;
+    });
   }
 
-  _blastAtom((int, int) position) {
-    world[position.$1][position.$2] = 0;
+  Future<void> _blastAtom((int, int) position, bool playerOne) async {
+    world[position.$1][position.$2] = '0';
+    loading = true;
+    await Future.delayed(const Duration(milliseconds: 150));
+    loading = false;
+
     // TOP
     if (_checkInBounds((position.$1 - 1, position.$2))) {
-      _selectTile((position.$1 - 1, position.$2));
+      _selectTile((position.$1 - 1, position.$2), true, playerOne);
     }
     // BOTTOM
     if (_checkInBounds((position.$1 + 1, position.$2))) {
-      _selectTile((position.$1 + 1, position.$2));
+      _selectTile((position.$1 + 1, position.$2), true, playerOne);
     }
     // RIGHT
     if (_checkInBounds((position.$1, position.$2 + 1))) {
-      _selectTile((position.$1, position.$2 + 1));
+      _selectTile((position.$1, position.$2 + 1), true, playerOne);
     }
     // LEFT
     if (_checkInBounds((position.$1, position.$2 - 1))) {
-      _selectTile((position.$1, position.$2 - 1));
+      _selectTile((position.$1, position.$2 - 1), true, playerOne);
     }
+
+    _checkEndGame();
   }
 
   bool _checkInBounds((int, int) position) {
@@ -100,9 +128,21 @@ class _AtomReactionState extends State<AtomReaction> {
       }
       return TilePos.side;
     }
-
     if (position.$1 == (totalTiles.$1 - 1)) {
       if (position.$2 == 0 || position.$2 == (totalTiles.$2 - 1)) {
+        return TilePos.corner;
+      }
+      return TilePos.side;
+    }
+
+    if (position.$2 == 0) {
+      if (position.$1 == 0 || position.$1 == (totalTiles.$1 - 1)) {
+        return TilePos.corner;
+      }
+      return TilePos.side;
+    }
+    if (position.$2 == (totalTiles.$2 - 1)) {
+      if (position.$1 == 0 || position.$1 == (totalTiles.$1 - 1)) {
         return TilePos.corner;
       }
       return TilePos.side;
@@ -110,16 +150,61 @@ class _AtomReactionState extends State<AtomReaction> {
     return TilePos.center;
   }
 
-  void restartGame() {}
+  void _checkEndGame() {
+    int countOne = 0;
+    int countTwo = 0;
 
-  Widget showAtom(int count) {
-    switch (count) {
-      case 1:
+    for (var row in world) {
+      for (var tile in row) {
+        if (tile == '0') {
+          continue;
+        }
+        if (tile.contains('-')) {
+          countTwo++;
+        } else {
+          countOne++;
+        }
+      }
+    }
+
+    setState(() {
+      if (countTwo == 0) {
+        _winner = true;
+      } else if (countOne == 0) {
+        _winner = false;
+      }
+    });
+  }
+
+  void restartGame() {
+    world = [];
+    for (int i = 0; i < totalTiles.$1; i++) {
+      world.add([]);
+      for (int j = 0; j < totalTiles.$2; j++) {
+        world[i].add('0');
+      }
+    }
+    setState(() {
+      _winner = null;
+      _playerOne = true;
+      loading = false;
+    });
+  }
+
+  Widget showAtom(String value) {
+    switch (value) {
+      case '1':
         return _singleAtom();
-      case 2:
+      case '-1':
+        return _singleAtom2();
+      case '2':
         return _doubleAtom();
-      case 3:
+      case '-2':
+        return _doubleAtom2();
+      case '3':
         return _threeAtom();
+      case '-3':
+        return _threeAtom2();
       default:
         return const SizedBox.shrink();
     }
@@ -134,7 +219,7 @@ class _AtomReactionState extends State<AtomReaction> {
             Positioned(
               left: (MediaQuery.of(context).size.width / 2) - (_iconSize / 2),
               top: MediaQuery.of(context).size.height / 15,
-              child: gameEnd
+              child: _winner != null
                   ? SizedBox(
                       child: IconButton(
                         iconSize: _iconSize,
@@ -144,12 +229,14 @@ class _AtomReactionState extends State<AtomReaction> {
                     )
                   : const SizedBox.shrink(),
             ),
-            const Positioned.fill(
+            Positioned.fill(
               child: Align(
                 alignment: Alignment.topCenter,
                 child: Text(
-                  'Atom Reaction',
-                  style: TextStyle(
+                  _winner != null
+                      ? 'Player ${_winner == true ? 1 : 2} won'
+                      : 'Atom Reaction',
+                  style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 30,
                       fontFamily: 'Arial'),
@@ -159,6 +246,7 @@ class _AtomReactionState extends State<AtomReaction> {
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Text(_playerOne ? 'Player 1' : 'Player 2'),
                 Padding(
                   padding: const EdgeInsets.all(32),
                   child: SizedBox(
@@ -196,7 +284,10 @@ class _AtomReactionState extends State<AtomReaction> {
     return Expanded(
       child: GestureDetector(
         onTap: () {
-          _selectTile(position);
+          if (loading || _winner != null) {
+            return;
+          }
+          _selectTile(position, false, _playerOne);
         },
         child: Container(
           margin: const EdgeInsets.all(2),
@@ -211,13 +302,19 @@ class _AtomReactionState extends State<AtomReaction> {
     );
   }
 
-  Widget _singleAtom() => Container(
+  Container _singleAtom() => Container(
         decoration:
             const BoxDecoration(color: Colors.red, shape: BoxShape.circle),
         width: 20,
       );
 
-  Widget _doubleAtom() => SizedBox(
+  Container _singleAtom2() => Container(
+        decoration:
+            const BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+        width: 20,
+      );
+
+  SizedBox _doubleAtom() => SizedBox(
         width: 30,
         child: Stack(
           children: [
@@ -241,7 +338,31 @@ class _AtomReactionState extends State<AtomReaction> {
         ),
       );
 
-  Widget _threeAtom() => SizedBox(
+  SizedBox _doubleAtom2() => SizedBox(
+        width: 30,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.blue, shape: BoxShape.circle),
+                width: 20,
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.blue, shape: BoxShape.circle),
+                width: 20,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  SizedBox _threeAtom() => SizedBox(
         width: 30,
         height: 30,
         child: Stack(
@@ -267,6 +388,39 @@ class _AtomReactionState extends State<AtomReaction> {
               child: Container(
                 decoration: const BoxDecoration(
                     color: Colors.red, shape: BoxShape.circle),
+                height: 20,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  SizedBox _threeAtom2() => SizedBox(
+        width: 30,
+        height: 30,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.blue, shape: BoxShape.circle),
+                width: 20,
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.blue, shape: BoxShape.circle),
+                width: 20,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.blue, shape: BoxShape.circle),
                 height: 20,
               ),
             ),
